c2ff8598e7e40c8fc78d45cd08cd0185a32df78e
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-10-25 09:05:34 +0200

    feat: add listings list

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -6,9 +6,37 @@ import bathtubIcon from "../assets/svg/bathtubIcon.svg"
 
 
 
-const ListingItem = () => {
+const ListingItem = ({listing, id, onEdit, onDelete}) => {
   return (
-    <div>ListingItem</div>
+    <li className="categoryListing">
+      <Link to={`/category/${listing.type}/${id}`} className="categoryListingLink">
+        <img src={listing.imageUrls[0]} alt={listing.name} className="categoryListingImg" />
+        <div className="categoryListingDetails">
+          <p className="categoryListingLocation">
+            {listing.location}
+          </p>
+          <p className="categoryListingName">
+            {listing.name}
+          </p>
+          <p className="categoryListingPrice">
+            ${listing.offer ? listing.discountedPrice.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',') : listing.regularPrice.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && ' / Month'}
+          </p>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bedroom" />
+            <p className="categoryListingInfoText">{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `${listing.bedrooms} Bedroom`}</p>
+            <img src={bathtubIcon} alt="bathroom" />
+            <p className="categoryListingInfoText">{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : `${listing.bathrooms} Bathroom`}</p>
+          </div>
+        </div>
+      </Link>
+
+      {onDelete && (
+        <DeleteIcon className="removeIcon" fill='rgb(231,76,60)' onClick={() => onDelete(listing.id, listing.name)}/>
+      )}
+    </li>
   )
 }
 
